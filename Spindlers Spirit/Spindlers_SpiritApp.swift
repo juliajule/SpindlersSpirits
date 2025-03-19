b1dfc7ae48359e127d8b9efb8384a489a5057863
PuhlmannJulia <s0584638@htw-berlin.de>
2025-03-19 14:17:20 +0100

    Switch to iOS App

---
+++ Spindlers Spirit/Spindlers_SpiritApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spindlers_SpiritApp.swift
+//  Spindlers Spirit
+//
+//  Created by Julia Puhlmann on 19.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Spindlers_SpiritApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
