db3cb7f948d359b55f34474a8d4ed37d784eb862
PuhlmannJulia <s0584638@htw-berlin.de>
2025-04-13 08:04:34 +0200

    Basic Tab View

---
+++ Spindlers Spirit/Screens/HomeTabView.swift
@@ -0,0 +1,41 @@
+//
+//  HomeTabView.swift
+//  Spindlers Spirit
+//
+//  Created by Julia Puhlmann on 07.04.25.
+//
+
+import SwiftUI
+
+struct HomeTabView: View {
+    
+    var body: some View {
+        TabView {
+            Text("verkostung")
+                .tabItem {
+                    Label("Verkostungen", systemImage: "person.3")
+                }
+            Text("whisky")
+                .tabItem {
+                    Label {
+                        Text("Whisky")
+                    } icon: {
+                        Image("bottle")
+                            .renderingMode(.template)
+                    }
+                }
+            Text("fav")
+                .tabItem {
+                    Label("Favoriten", systemImage: "heart")
+                }
+            Text("search")
+                .tabItem {
+                    Label("Suche", systemImage: "magnifyingglass")
+                }
+        }
+    }
+}
+
+#Preview {
+    HomeTabView()
+}
---
+++ Spindlers Spirit/Views/Empty State/EmptyState.swift
@@ -0,0 +1,38 @@
+//
+//  EmptyState.swift
+//  Spindlers Spirit
+//
+//  Created by Julia Puhlmann on 07.04.25.
+//
+
+import SwiftUI
+
+struct EmptyState: View {
+    
+    let imageName: String
+    let message: String
+    
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .ignoresSafeArea(edges: .all)
+            VStack {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 150)
+                Text(message)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .padding()
+            }
+            .offset(y: -50)
+        }
+    }
+}
+
+#Preview {
+    EmptyState(imageName: "empty-order", message: "Your order is empty")
+}
